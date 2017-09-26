5c72bbb1c93f75e342347313d28b0f21f0246900
Anonymous <k.buzzard@imperial.ac.uk>
2017-09-26 10:35:56 +0100

    All of sheet 1 Q1 now typed into Lean

--- M1F/2017-18/Example_Sheet_01/Question_01/question.lean
+++ M1F/2017-18/Example_Sheet_01/Question_01/question.lean
@@ -1,18 +1,48 @@
 /-
-M1F Sheet 1 Question 1 part (a) 
+M1F 2017-18 Sheet 1 Question 1
 Author : Kevin Buzzard
 
-Preliminary version
+Note: this version of the question uses integers instead of real numbers.
+This is because the real numbers are not currently in Lean's standard library
+and I want to make things as easy as possible. This file should work with
+any version of lean -- whether you installed it yourself or are running
+the version on https://leanprover.github.io/live/latest/
 
-TODO (KMB) : Replace topology.real with more user-friendly real numbers.
-           : figure out how to use x^2 instead of x*x
-           : figure out how to make 3 mean 3:R rather than 3:nat
-           : remove some of those stupid brackets round the 3:R's somehow.
-           : This is actually the 2016-17 example sheet question; update later.
+Lean does have real numbers -- it's just that you need to install
+another library to get them (the mathlib library).
+
+TODO (KMB) : Figure out how to get the students to do an import.
+           : Then (*) switch to real numbers.
+           :      (*) use x^2 instead of x*x
 -/
 
-import topology.real
+-- part (a): which one of these is provable? Prove one, comment
+-- out the other.
 
-open real
+theorem m1f_sheet01_q01a_is_T : ∀ x : ℤ, x*x - 3*x + 2 = 0 → x=1 := sorry
+theorem m1f_sheet01_q01a_is_F : ¬ (∀ x : ℤ, x*x - 3*x + 2 = 0 → x=1) := sorry
 
-theorem m1f_sheet01_q01 : ¬ (∀ x : ℝ,(x*x-(3:ℝ)*x+(2:ℝ)=(0:ℝ) → x=(1:ℝ))) := sorry
+-- part (b)
+
+theorem m1f_sheet01_q01b_is_T : ∀ x : ℤ, x=1 → x*x-3*x+2=0 := sorry
+theorem m1f_sheet01_q01b_is_F : ¬ (∀ x : ℤ, x=1 → x*x-3*x+2=0) := sorry
+
+-- part (c)
+
+theorem m1f_sheet01_q01c_is_T : ∀ x : ℤ, x*x - 3*x + 2 = 0 ↔ x=1 := sorry
+theorem m1f_sheet01_q01c_is_F : ¬ (∀ x : ℤ, x*x - 3*x + 2 = 0 ↔ x=1) := sorry
+
+-- part (d)
+
+theorem m1f_sheet01_q01d_is_T : ∀ x : ℤ, x*x - 3*x + 2 = 0 ↔ (x=1 ∨ x=2) := sorry
+theorem m1f_sheet01_q01d_is_F : ¬ (∀ x : ℤ, x*x - 3*x + 2 = 0 ↔ (x=1 ∨ x=2)) := sorry
+
+-- part (e)
+
+theorem m1f_sheet01_q01e_is_T : ∀ x : ℤ, x*x - 3*x + 2 = 0 → (x=1 ∨ x=2 ∨ x=3) := sorry
+theorem m1f_sheet01_q01e_is_F : ¬ (∀ x : ℤ, x*x - 3*x + 2 = 0 → (x=1 ∨ x=2 ∨ x=3)) := sorry
+
+-- part (f)
+
+theorem m1f_sheet01_q01f_is_T : ∀ x : ℤ, (x=1 ∨ x=2 ∨ x=3) → x*x - 3*x + 2 = 0  := sorry
+theorem m1f_sheet01_q01f_is_F : ¬ (∀ x : ℤ, (x=1 ∨ x=2 ∨ x=3) → x*x - 3*x + 2 = 0)  := sorry
