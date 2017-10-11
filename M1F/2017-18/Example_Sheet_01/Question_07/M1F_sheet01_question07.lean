ce52e3df10f653ea3509e2b2f385b7ea6e9de063
Anonymous <k.buzzard@imperial.ac.uk>
2017-10-12 00:58:14 +0100

    Added Q7

--- M1F/2017-18/Example_Sheet_01/Question_07/M1F_sheet01_question07.lean
+++ M1F/2017-18/Example_Sheet_01/Question_07/M1F_sheet01_question07.lean
@@ -1,3 +1,16 @@
-import topology.real
+import analysis.real
+-- real numbers live in here as of 1st Oct 2017.
+-- NB you need the current mathlib installed to get this working.
 
--- def A : set ℝ := { x ∈ R | x^2 < 3}
+-- of_rat is the injection from the rationals to the reals.#check
+
+def A : set ℝ := { x | x^2 < 3}
+def B : set ℝ := {x | (∃ y : ℤ, x = of_rat y) ∧ x^2 < 3}
+def C : set ℝ := {x | x^3 < 3}
+
+theorem part_a : of_rat (1/2) ∈ A ∩ B := sorry
+theorem part_b : of_rat (1/2) ∈ A ∪ B := sorry
+theorem part_c : A ⊆ C := sorry
+theorem part_d : B ⊆ C := sorry
+theorem part_e : C ⊆ A ∪ B := sorry
+theorem part_f : (A ∩ B) ∪ C = (A ∪ B) ∩ C := sorry
