a695e500f57c47be54093090572e78c56491a834
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2017-11-14 19:17:54 +0000

    finished proof that complexes are a field

--- xenalib/complex.lean
+++ xenalib/complex.lean
@@ -31,10 +31,9 @@ split,
 exact eq_of_re_eq_and_im_eq _ _,
 end
 
-theorem proj_re (r0 i0 : real) : (complex.mk r0 i0).re = r0 := rfl
-theorem proj_im (r0 i0 : real) : (complex.mk r0 i0).im = i0 := rfl
+lemma proj_re (r0 i0 : real) : (complex.mk r0 i0).re = r0 := rfl
+lemma proj_im (r0 i0 : real) : (complex.mk r0 i0).im = i0 := rfl
 
--- do I also add proj_re and proj_im?
 
 local attribute [simp] eq_iff_re_eq_and_im_eq proj_re proj_im
 
@@ -63,7 +62,7 @@ def neg : complex → complex :=
 
 def mul : complex → complex → complex :=
 λ z w, { re := z.re*w.re - z.im*w.im,
-         im := z.im*w.re + z.re*w.im}
+         im := z.re*w.im + z.im*w.re}
 
 def norm_squared : complex → real :=
 λ z, z.re*z.re+z.im*z.im
@@ -79,9 +78,41 @@ instance : has_mul complex := ⟨complex.mul⟩
 instance : has_inv complex := ⟨complex.inv⟩
 instance : has_div ℝ := ⟨λx y, x * y⁻¹⟩
 
+
+lemma proj_add_re (z w: complex) : (z+w).re=z.re+w.re := rfl
+lemma proj_add_im (z w: complex) : (z+w).im=z.im+w.im := rfl
+lemma proj_neg_re (z: complex) : (-z).re=-z.re := rfl
+lemma proj_neg_im (z: complex) : (-z).im=-z.im := rfl
+lemma proj_mul_re (z w: complex) : (z*w).re=z.re*w.re-z.im*w.im := rfl
+lemma proj_mul_im (z w: complex) : (z*w).im=z.re*w.im+z.im*w.re := rfl
+lemma proj_of_real_re (r:real) : (of_real r).re = r := rfl
+lemma proj_of_real_im (r:real) : (of_real r).im = 0 := rfl
+local attribute [simp] proj_add_re proj_add_im proj_neg_re proj_neg_im 
+local attribute [simp] proj_mul_re proj_mul_im proj_of_real_re proj_of_real_im
+
+lemma norm_squared_pos_of_nonzero (z : complex) (H : z ≠ 0) : norm_squared z > 0 :=
+begin -- far more painful than it should be but I need it for inverses
+suffices : z.re ≠ 0 ∨ z.im ≠ 0,
+  apply lt_of_le_of_ne,
+    exact add_nonneg (mul_self_nonneg _) (mul_self_nonneg _),
+  intro H2,
+  cases this with Hre Him,
+    exact Hre (eq_zero_of_mul_self_add_mul_self_eq_zero (eq.symm H2)),
+  unfold norm_squared at H2,rw [add_comm] at H2,
+  exact Him (eq_zero_of_mul_self_add_mul_self_eq_zero (eq.symm H2)),
+have : ¬ (z.re = 0 ∧ z.im = 0),
+  intro H2,
+  exact H (eq_of_re_eq_and_im_eq z 0 H2),
+cases classical.em (z.re = 0) with Hre_eq Hre_ne,
+  right,
+  intro H2,
+  apply this,
+  exact ⟨Hre_eq,H2⟩,
+left,assumption,
+end
+
 -- I don't know how to set up
--- real.cast_zero etc (look to see
--- how it's done in real.lean?)
+-- real.cast_zero etc
 
 lemma of_real_injective : function.injective of_real :=
 begin
@@ -96,26 +127,24 @@ lemma of_real_one : (1:complex) = of_real 1 := rfl
 -- set_option trace.simplify true
 -- set_option pp.notation false
 
+/- how does one do this??
+meta def splat : tactic unit :=
+begin
+rw [eq_iff_re_eq_and_im_eq],
+split;simp
+end
+-/
+
 lemma of_real_neg (r : real) : -of_real r = of_real (-r) := 
 begin
 rw [eq_iff_re_eq_and_im_eq],
-split,
--- real part works fine as it's -r = -r
-  refl,
--- imag part is -0 = 0 which needs more hints.
-suffices : -(0:ℝ)=0,
-exact this,
-simp,
+split;simp
 end
 
 lemma of_real_add (r s: real) : of_real r + of_real s = of_real (r+s) :=
 begin
 rw [eq_iff_re_eq_and_im_eq],
-split,
-  refl,
-suffices : (0:ℝ)+0=0,
-exact this,
-simp,
+split;simp
 end
 
 lemma of_real_sub (r s:real) : of_real r - of_real s = of_real(r-s) :=
@@ -131,16 +160,9 @@ end
 lemma of_real_mul (r s:real) : of_real r * of_real s = of_real (r*s) :=
 begin
 rw [eq_iff_re_eq_and_im_eq],
-split,
-  suffices : r*s-0*0=r*s,
-  exact this,
-  simp,
-suffices : 0*s+r*0=0,
-exact this,
-simp
+split;simp,
 end
 
--- set_option pp.notation false
 lemma of_real_inv (r:real) : (of_real r)⁻¹ = of_real (r⁻¹) :=
 begin
 rw [eq_iff_re_eq_and_im_eq],
@@ -175,29 +197,28 @@ instance : add_comm_group complex :=
   neg          := has_neg.neg,
   zero_add     := begin
     intro z,
-    apply eq_of_re_eq_and_im_eq,
-    split;apply zero_add
+    rw eq_iff_re_eq_and_im_eq,
+    split;simp
   end,
   add_zero     := begin
     intro z,
-    apply eq_of_re_eq_and_im_eq,
-    split;apply add_zero
+    rw eq_iff_re_eq_and_im_eq,
+    split;simp
   end,
   add_comm     := begin
     intros,
-    apply eq_of_re_eq_and_im_eq,
-    split;apply add_comm,
-  end
-  ,
+    rw eq_iff_re_eq_and_im_eq,
+    split;simp
+  end,
   add_assoc    := begin
-    intros a b c,
-    apply eq_of_re_eq_and_im_eq,
-    split;apply add_assoc,
+    intros,
+    rw eq_iff_re_eq_and_im_eq,
+    split;simp,
   end,
   add_left_neg := begin
     intros,
     apply eq_of_re_eq_and_im_eq,
-    split;apply add_left_neg,
+    split;simp,
   end
 }
 
@@ -209,39 +230,83 @@ instance : discrete_field complex :=
   mul_one          := begin
     intros,
     apply eq_of_re_eq_and_im_eq,
-    split,
-      unfold has_mul.mul,
-      unfold semigroup.mul,
-      unfold has_mul.mul,
-      unfold mul,
-
-    simp [proj_re],
-
-    unfold has_mul.mul semigroup.mul,
-    apply eq_of_re_eq_and_im_eq,
-    split;unfold mul,
-    rw proj_re,
-
-      unfold has_mul.mul semigroup.mul mul,
-      rw proj_re,
-      change (1:complex).re with (1:ℝ),
-
-        rw mul_one, -- dammit
-      rw [mul_one],
+    split;simp,
   end,
-  one_mul          := sorry,
-  mul_comm         := sorry,
-  mul_assoc        := sorry,
-  left_distrib     := sorry,
-  right_distrib    := sorry,
-  zero_ne_one      := sorry,
-  mul_inv_cancel   := sorry,
-  inv_mul_cancel   := sorry,
-  inv_zero         := sorry,
+  one_mul          := begin
+    intros,
+    apply eq_of_re_eq_and_im_eq,
+    split;simp,
+  end,
+  mul_comm         := begin
+    intros,
+    apply eq_of_re_eq_and_im_eq,
+    split;simp,
+  end,
+  mul_assoc        := begin
+    intros,
+    apply eq_of_re_eq_and_im_eq,
+    split;simp [add_mul,mul_add],
+  end,
+  left_distrib     := begin
+    intros,
+    apply eq_of_re_eq_and_im_eq,
+    split;simp [add_mul,mul_add,add_comm_group.add],
+  end,
+  right_distrib    := begin
+    intros,
+    apply eq_of_re_eq_and_im_eq,
+    split;simp [add_mul,mul_add,add_comm_group.add],
+  end,
+  zero_ne_one      := begin 
+    intro H,
+    suffices : (0:complex).re = (1:complex).re,
+      revert this,
+      apply zero_ne_one,
+    rw [←H],
+    trivial,
+    end,
+  mul_inv_cancel   := begin
+    intros z H,
+    have H2 : norm_squared z ≠ 0 := ne_of_gt (norm_squared_pos_of_nonzero z H),
+    apply eq_of_re_eq_and_im_eq,
+    unfold has_inv.inv inv,
+    rw [proj_mul_re,proj_mul_im],
+    split,
+      suffices : z.re*(z.re/norm_squared z) + -z.im*(-z.im/norm_squared z) = 1,
+        by simpa,
+      rw [←mul_div_assoc,←mul_div_assoc,neg_mul_neg,div_add_div_same],
+      unfold norm_squared at *,
+      exact div_self H2,
+    suffices : z.im * (z.re / norm_squared z) + z.re * (-z.im / norm_squared z) = 0,
+      by simpa,
+    rw [←mul_div_assoc,←mul_div_assoc,div_add_div_same],
+    simp [zero_div],
+  end,
+  inv_mul_cancel   := begin -- let's try cut and pasting mul_inv_cancel proof
+    intros z H,
+    have H2 : norm_squared z ≠ 0 := ne_of_gt (norm_squared_pos_of_nonzero z H),
+    apply eq_of_re_eq_and_im_eq,
+    unfold has_inv.inv inv,
+    rw [proj_mul_re,proj_mul_im],
+    split,
+      suffices : z.re*(z.re/norm_squared z) + -z.im*(-z.im/norm_squared z) = 1,
+        by simpa,
+      rw [←mul_div_assoc,←mul_div_assoc,neg_mul_neg,div_add_div_same],
+      unfold norm_squared at *,
+      exact div_self H2,
+    suffices : z.im * (z.re / norm_squared z) + z.re * (-z.im / norm_squared z) = 0,
+      by simpa,
+    rw [←mul_div_assoc,←mul_div_assoc,div_add_div_same],
+    simp [zero_div],
+  end, -- it worked without modification!
+  inv_zero         := begin
+  unfold has_inv.inv inv add_comm_group.zero,
+  apply eq_of_re_eq_and_im_eq,
+  split;simp [zero_div],
+  end,
   has_decidable_eq := by apply_instance }
 
--- instance : topological_ring complex :=
--- { real.topological_add_group with continuous_mul := continuous_mul_real }
+-- instance : topological_ring complex := missing
 
 end complex
 
