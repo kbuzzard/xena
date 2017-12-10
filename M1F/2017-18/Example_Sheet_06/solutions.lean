6dbe85cad3b3cfa784c9579f2598527662d5754d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2017-12-10 15:51:05 +0000

    Q3a done

--- M1F/2017-18/Example_Sheet_06/solutions.lean
+++ M1F/2017-18/Example_Sheet_06/solutions.lean
@@ -1,7 +1,22 @@
-import analysis.real 
+import analysis.real tactic.norm_num
 
 theorem Q1 : 1=1 := sorry
 theorem Q2 : 1=1 := sorry
+
+lemma avg_lt_max {mn mx: ℝ} (H : mn < mx) : (mn+mx) / 2 < mx :=
+begin
+  apply (mul_lt_mul_right (show (0:ℝ)<2, by norm_num)).1,
+  rw [div_mul_cancel _ (two_ne_zero)],
+  simp [H,mul_two],
+end
+
+lemma min_lt_avg {mn mx: ℝ} (H : mn < mx) : mn < (mn+mx) / 2 :=
+begin
+  apply (mul_lt_mul_right (show (0:ℝ)<2, by norm_num)).1,
+  rw [div_mul_cancel _ (two_ne_zero)],
+  simp [H,mul_two],
+end
+
 
 def S3a : set ℝ := {x : ℝ | x<0} 
 
@@ -10,18 +25,23 @@ begin
 unfold is_lub,
 unfold is_least,
 split,
-  unfold upper_bounds,
+{ unfold upper_bounds,
   intro a,
-  exact le_of_lt,
+  exact le_of_lt },
 unfold lower_bounds,
 intro b,
 intro Hb,
 refine le_of_not_gt _,
 intro Hnb,
-let c:=b/2,
+let c:=(b+0)/2,
 unfold upper_bounds at Hb,
 have cont := Hb c,
 clear Hb,
 have H : c ∈ S3a,
-apply hmm...
+{ exact avg_lt_max Hnb,
+},
+have Hcleb := cont H,
+have Hbltc : b < c := min_lt_avg Hnb,
+exact not_lt_iff.2 Hcleb Hbltc,
 end
+
