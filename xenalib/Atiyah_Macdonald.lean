0a00ada9a70161927c7dc1bb1fa7f0dc1b662fd2
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-01-20 14:24:48 +0000

    quotient groups

--- xenalib/Atiyah_Macdonald.lean
+++ xenalib/Atiyah_Macdonald.lean
@@ -1,6 +1,8 @@
 noncomputable theory
 local attribute [instance] classical.prop_decidable
 
+namespace add_comm_group
+
 class is_add_subgroup {M : Type*} [add_comm_group M] (N : set M) : Prop :=
 (zero : (0:M) ∈ N)
 (add  : ∀ {x y}, x ∈ N → y ∈ N → x + y ∈ N)
@@ -42,6 +44,7 @@ theorem quot_map_is_group_hom (M : Type*) [add_comm_group M] (N : set M) [is_add
   quot.mk r (a+b) = add_quot_group_add (quot.mk r a) (quot.mk r b) :=
 begin
 apply quot.sound,
+unfold add_quot_group_reln,
 simp [is_add_subgroup.zero],
 end 
 
@@ -57,22 +60,11 @@ instance add_quot_group_is_group {M : Type*} [add_comm_group M] {N : set M} [is_
       intro b,
       refine quot.ind _,
       intro c,
-      simp [add_assoc,quot_map_is_group_hom],
-      intros a b c,
-      rw quot_map_is_group_hom,
-      show quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _
-      (quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ a b)
-      c =
-    quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ a
-      (quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ b c),
-
-
-      apply congr_fun,
-            show  quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _
-      (quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ a b) =
-    λ (c : add_quot_group M N),
-      quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ a
-        (quot.lift (λ (m₁ : M), quot.lift (λ (m₂ : M), quot.mk r (m₁ + m₂)) _) _ b c),
+      unfold add_quot_group_add,
+      suffices : quot.mk (add_quot_group_reln M N) (a + b + c) = quot.mk (add_quot_group_reln M N) (a + (b + c)),
+        simp [quot_map_is_group_hom,this],
+      rw [add_assoc],
+      refl,
     end,
     zero := quot.mk r 0,
     neg := quot.lift (λ m : M, quot.mk r (-m)) (begin
@@ -82,14 +74,61 @@ instance add_quot_group_is_group {M : Type*} [add_comm_group M] {N : set M} [is_
       have H : -(a-b) ∈ N := is_add_subgroup.neg HabN,
       have H2 : -a - -b = -(a-b) := by simp,
       rwa [H2],
-      end),
-    
+    end),
+    zero_add := begin
+      refine quot.ind _,
+      intro a,
+      apply quot.sound,
+      unfold add_quot_group_reln,
+      simp [sub_self],
+      exact is_add_subgroup.zero _,
+    end,
+    add_zero := begin
+      refine quot.ind _,
+      intro a,
+      apply quot.sound,
+      unfold add_quot_group_reln,
+      simp [sub_self],
+      exact is_add_subgroup.zero _,
+    end,
+    add_left_neg := begin
+      refine quot.ind _,
+      intro a,
+      apply quot.sound,
+      rw neg_add_self,
+      unfold add_quot_group_reln,
+      rw sub_self,
+      exact is_add_subgroup.zero _,
+    end,
+    add_comm := begin
+      refine quot.ind _,
+      intro a,
+      refine quot.ind _,
+      intro b,
+      show add_quot_group_add ( quot.mk (add_quot_group_reln M N) a) (quot.mk (add_quot_group_reln M N) b )
+        = add_quot_group_add ( quot.mk (add_quot_group_reln M N) b) (quot.mk (add_quot_group_reln M N) a ),
+      rw [eq.symm (quot_map_is_group_hom M N a b)],
+      rw [eq.symm (quot_map_is_group_hom M N b a)],
+      rw [add_comm]
+    end,
   }
 
+def subgroup_to_quot_subgroup {M : Type*} [add_comm_group M] {N : set M} [is_add_subgroup N] (I : set M)
+  : set (add_quot_group M N) :=
+  quot.mk ' I -- or something
 
 
 
---class is_ideal {R : Type*} [comm_ring R] (J : set R) : Prop :=
---(zero : (0:R) ∈ J)
---(add  : ∀ {x y}, x ∈ J → y ∈ J → x + y ∈ J)
---(mul : ∀ r x : R, x ∈ J → r * x ∈ J)
+end add_comm_group
+
+namespace comm_ring
+
+class is_ideal {R : Type*} [comm_ring R] (J : set R) : Prop :=
+(zero : (0:R) ∈ J)
+(add  : ∀ {x y}, x ∈ J → y ∈ J → x + y ∈ J)
+(mul : ∀ r x : R, x ∈ J → r * x ∈ J)
+
+
+
+
+end comm_ring
