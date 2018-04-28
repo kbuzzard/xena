48ce83d8bc9782a08ee852d126e784f696086846
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-28 18:11:49 +0100

    constants proof and class proof

---
+++ canonical_isomorphism/group_axioms_constants.lean
@@ -0,0 +1,59 @@
+-- do I use a section? What even is a section?
+-- so I use a namespace? What would a sensible convention be?
+-- I don't care about junk like imports at the beginning of files
+namespace xena
+
+
+-- This file is for undergraduate mathematicians who want to see the 
+-- proof that one of the axioms that Lean uses to define a group
+-- actually follows from the others!
+
+-- G comes with notation * (group law) 1 (identiy) and a⁻¹ (inverse)
+-- mul : G → G → G := λ g h, g * h
+-- one : G := 1
+-- inv : G → G := λ a, a⁻¹
+
+constants {G : Type}
+variables [has_mul G] [has_one G] [has_inv G] 
+-- using constants seemed to break type class inference
+
+constants 
+(mul_assoc : ∀ {a b c : G}, a * b * c = a * (b * c))
+(one_mul : ∀ {a : G}, 1 * a = a)
+(mul_left_inv : ∀ {a : G}, a⁻¹ * a = 1)
+
+-- if I use (a b c : G) I end up with _s everywhere in the calc proof
+-- that I don't know how to avoid.
+
+-- This comes out nicely
+lemma mul_left_cancel : ∀ {a b c : G}, a * b = a * c → b = c := 
+begin
+  intros a b c Habac,
+  exact calc b = 1 * b : (one_mul).symm
+           ... = (a⁻¹ * a) * b : by rw [mul_left_inv] -- why does that line even work? I believe it should fail. Is it a bug?
+           ... = a⁻¹ * (a * b) : mul_assoc
+           ... = a⁻¹ * (a * c) : by rw Habac
+           ... = (a⁻¹ * a) * c : (mul_assoc).symm
+           ... = 1 * c : by rw [mul_left_inv]
+           ... = c : one_mul
+end
+
+-- No blanks at all! Unification enabled us not to
+-- have to tell Lean about the inputs to mul_left_inv. The fact that the
+-- calc proof was so explicit means that unification is powerful.
+
+theorem group'.mul_one : ∀ (a : G), a * 1 = a :=
+begin
+intro a,
+ apply @mul_left_cancel _ _ _ a⁻¹ _ _, -- dammit. 
+ rw [←mul_assoc,mul_left_inv,one_mul],
+end 
+
+-- and it was going so well.
+
+#exit
+-- thoughts about canonical isomorphism
+import data.equiv
+theorem group'_canonically_isomorphic_to_group (G : Type) : equiv [group' G] [group G] :=
+⟨_,_,_,_⟩
+
