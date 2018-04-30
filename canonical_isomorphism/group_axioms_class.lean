aec4489d24547182b2a6b69517d8fc94c994e452
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-30 22:37:09 +0100

    finished group theory blog post.

--- canonical_isomorphism/group_axioms_class.lean
+++ canonical_isomorphism/group_axioms_class.lean
@@ -1,10 +1,6 @@
--- do I use a section? What even is a section?
--- so I use a namespace? What would a sensible convention be?
--- I don't care about junk like imports at the beginning of files
-
 -- This file is for undergraduate mathematicians who want to see the 
 -- proof that one of the axioms that Lean uses to define a group
--- actually follows from the others!
+-- actually follows from the others.
 
 -- G comes with notation * (group law) 1 (identiy) and a⁻¹ (inverse)
 -- mul : G → G → G := λ g h, g * h
@@ -17,40 +13,29 @@ class group' (G : Type) extends has_group_notation G :=
 (mul_assoc : ∀ (a b c : G), a * b * c = a * (b * c))
 (one_mul : ∀ (a : G), 1 * a = a)
 (mul_left_inv : ∀ (a : G), a⁻¹ * a = 1)
+-- Lean 3.4.1 also uses mul_one : ∀ (a : G), a * 1 = a , but we'll see we can deduce it!
+-- Note : this doesn't matter at all :-)
 
+namespace group'
 variables {G : Type} [group' G]  
--- variables (H : Type) [has_mul H] [has_one H] [has_inv H]
 
 -- We prove left_mul_cancel for group'
 
-lemma group'.mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
-begin
-  intros a b c Habac,
-  exact calc b = 1 * b : (group'.one_mul _).symm
-           ... = (a⁻¹ * a) * b : by rw [←group'.mul_left_inv _]
-           ... = a⁻¹ * (a * b) : group'.mul_assoc _ _ _
-           ... = a⁻¹ * (a * c) : by rw Habac
-           ... = (a⁻¹ * a) * c : (group'.mul_assoc _ _ _).symm
-           ... = 1 * c : by rw [group'.mul_left_inv a]
-           ... = c : group'.one_mul _
-end
--- why do I seem to have to fill in far more blanks with _ in term mode?
+lemma mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
+λ (a b c : G) (Habac : a * b = a * c), -- got to deduce b = c.
+ calc b = 1 * b         : by rw one_mul
+    ... = (a⁻¹ * a) * b : by rw mul_left_inv
+    ... = a⁻¹ * (a * b) : by rw mul_assoc
+    ... = a⁻¹ * (a * c) : by rw Habac
+    ... = (a⁻¹ * a) * c : by rw mul_assoc
+    ... = 1 * c         : by rw mul_left_inv
+                ... = c : by rw one_mul
 
 theorem group'.mul_one : ∀ (a : G), a * 1 = a :=
 begin
-intro a,
- apply group'.mul_left_cancel a⁻¹,
- rw [←group'.mul_assoc,group'.mul_left_inv,group'.one_mul],
+intro a, -- goal is a * 1 = a
+ apply mul_left_cancel a⁻¹, -- goal now a⁻¹ * (a * 1) = a⁻¹ * a
+ rw [←mul_assoc,mul_left_inv,one_mul], -- apply each axiom once to finish!
 end 
 
--- other than the group' everywhere, I really like how this came out.
--- You can really clearly see the unique point in the proof where type
--- unification can't guess the variable.
-
-#exit
-
--- thoughts about canonical isomorphism
-import data.equiv
-theorem group'_canonically_isomorphic_to_group (G : Type) : equiv [group' G] [group G] :=
-⟨_,_,_,_⟩
-
+end group'
