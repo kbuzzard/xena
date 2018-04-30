8d2d1341975f60ce396bd787e3a3b06135fd3697
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-30 23:04:33 +0100

    old commit

--- canonical_isomorphism/group_axioms_class.lean
+++ canonical_isomorphism/group_axioms_class.lean
@@ -10,25 +10,28 @@
 -- mul : G → G → G := λ g h, g * h
 -- one : G := 1
 -- inv : G → G := λ a, a⁻¹
+universe u
 
-class has_group_notation (G : Type) extends has_mul G, has_one G, has_inv G
+class has_group_notation (G : Type u) extends has_mul G, has_one G, has_inv G
 
-class group' (G : Type) extends has_group_notation G :=
+class group' (G : Type u) extends has_group_notation G :=
 (mul_assoc : ∀ (a b c : G), a * b * c = a * (b * c))
 (one_mul : ∀ (a : G), 1 * a = a)
 (mul_left_inv : ∀ (a : G), a⁻¹ * a = 1)
 
-variables {G : Type} [group' G]  
+namespace group'
+
+variables {G : Type u} [group' G]  
 -- variables (H : Type) [has_mul H] [has_one H] [has_inv H]
 
 -- We prove left_mul_cancel for group'
 
-lemma group'.mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
+lemma mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
 begin
   intros a b c Habac,
-  exact calc b = 1 * b : (group'.one_mul _).symm
-           ... = (a⁻¹ * a) * b : by rw [←group'.mul_left_inv _]
-           ... = a⁻¹ * (a * b) : group'.mul_assoc _ _ _
+  exact calc b = 1 * b : (one_mul _).symm
+           ... = (a⁻¹ * a) * b : by rw [←mul_left_inv _]
+           ... = a⁻¹ * (a * b) : by rw [mul_assoc]
            ... = a⁻¹ * (a * c) : by rw Habac
            ... = (a⁻¹ * a) * c : (group'.mul_assoc _ _ _).symm
            ... = 1 * c : by rw [group'.mul_left_inv a]
@@ -36,11 +39,11 @@ begin
 end
 -- why do I seem to have to fill in far more blanks with _ in term mode?
 
-theorem group'.mul_one : ∀ (a : G), a * 1 = a :=
+theorem mul_one : ∀ (a : G) [group' G], a * 1 = a :=
 begin
 intro a,
- apply group'.mul_left_cancel a⁻¹,
- rw [←group'.mul_assoc,group'.mul_left_inv,group'.one_mul],
+ apply mul_left_cancel a⁻¹,
+ rw [←mul_assoc,mul_left_inv,one_mul],
 end 
 
 -- other than the group' everywhere, I really like how this came out.
