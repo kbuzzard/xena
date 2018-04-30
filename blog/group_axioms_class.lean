448f2cf2f3cdd93397d7b4f86fea103bb3e1ef6e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-30 22:42:53 +0100

    musing over Lean group axioms

---
+++ blog/group_axioms_class.lean
@@ -0,0 +1,41 @@
+-- This file is for undergraduate mathematicians who want to see the 
+-- proof that one of the axioms that Lean uses to define a group
+-- actually follows from the others.
+
+-- G comes with notation * (group law) 1 (identiy) and a⁻¹ (inverse)
+-- mul : G → G → G := λ g h, g * h
+-- one : G := 1
+-- inv : G → G := λ a, a⁻¹
+
+class has_group_notation (G : Type) extends has_mul G, has_one G, has_inv G
+
+class group' (G : Type) extends has_group_notation G :=
+(mul_assoc : ∀ (a b c : G), a * b * c = a * (b * c))
+(one_mul : ∀ (a : G), 1 * a = a)
+(mul_left_inv : ∀ (a : G), a⁻¹ * a = 1)
+-- Lean 3.4.1 also uses mul_one : ∀ (a : G), a * 1 = a , but we'll see we can deduce it!
+-- Note : this doesn't matter at all :-)
+
+namespace group'
+variables {G : Type} [group' G]  
+
+-- We prove left_mul_cancel for group'
+
+lemma mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
+λ (a b c : G) (Habac : a * b = a * c), -- got to deduce b = c.
+ calc b = 1 * b         : by rw one_mul
+    ... = (a⁻¹ * a) * b : by rw mul_left_inv
+    ... = a⁻¹ * (a * b) : by rw mul_assoc
+    ... = a⁻¹ * (a * c) : by rw Habac
+    ... = (a⁻¹ * a) * c : by rw mul_assoc
+    ... = 1 * c         : by rw mul_left_inv
+                ... = c : by rw one_mul
+
+theorem group'.mul_one : ∀ (a : G), a * 1 = a :=
+begin
+intro a, -- goal is a * 1 = a
+ apply mul_left_cancel a⁻¹, -- goal now a⁻¹ * (a * 1) = a⁻¹ * a
+ rw [←mul_assoc,mul_left_inv,one_mul], -- apply each axiom once to finish!
+end 
+
+end group'
---
+++ blog/group_axioms_other/group_axioms_sructure.lean
@@ -0,0 +1,57 @@
+-- do I use a section? What even is a section?
+-- so I use a namespace? What would a sensible convention be?
+-- I don't care about junk like imports at the beginning of files
+
+-- This file is for undergraduate mathematicians who want to see the 
+-- proof that one of the axioms that Lean uses to define a group
+-- actually follows from the others!
+
+-- G comes with notation * (group law) 1 (identiy) and a⁻¹ (inverse)
+-- mul : G → G → G := λ g h, g * h
+-- one : G := 1
+-- inv : G → G := λ a, a⁻¹
+
+class has_group_notation (G : Type) extends has_mul G, has_one G, has_inv G
+
+class group' (G : Type) extends has_group_notation G :=
+(mul_assoc : ∀ (a b c : G), a * b * c = a * (b * c))
+(one_mul : ∀ (a : G), 1 * a = a)
+(mul_left_inv : ∀ (a : G), a⁻¹ * a = 1)
+
+variables {G : Type} (group'.G : group' G) [has_group_notation G]  
+-- variables (H : Type) [has_mul H] [has_one H] [has_inv H]
+
+-- We prove left_mul_cancel for group'
+
+open group'
+lemma group'.mul_left_cancel : ∀ (a b c : G), a * b = a * c → b = c := 
+λ a b c Habac,
+ calc b = 1 * b         : by rw one_mul
+    ... = (a⁻¹ * a) * b : by rw mul_left_inv
+    ... = a⁻¹ * (a * b) : by rw mul_assoc
+    ... = a⁻¹ * (a * c) : by rw Habac
+    ... = (a⁻¹ * a) * c : by rw mul_assoc
+    ... = 1 * c         : by rw mul_left_inv
+                ... = c : by rw one_mul
+
+
+-- why do I seem to have to fill in far more blanks with _ in term mode?
+
+theorem group'.mul_one : ∀ (a : G), a * 1 = a :=
+begin
+intro a,
+ apply group'.mul_left_cancel a⁻¹,
+ rw [←group'.mul_assoc,group'.mul_left_inv,group'.one_mul],
+end 
+
+-- other than the group' everywhere, I really like how this came out.
+-- You can really clearly see the unique point in the proof where type
+-- unification can't guess the variable.
+
+#exit
+
+-- thoughts about canonical isomorphism
+import data.equiv
+theorem group'_canonically_isomorphic_to_group (G : Type) : equiv [group' G] [group G] :=
+⟨_,_,_,_⟩
+
