8bd6fb716aecf9e39b6c791d32549d15dfb75c99
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-06-13 21:33:25 +0100

    adding comments

--- blog/baby_ring.lean
+++ blog/baby_ring.lean
@@ -3,28 +3,15 @@ import tactic.basic data.num.lemmas data.list.basic
 namespace ring_tac
 open tactic
 
--- why this line?
-@[derive has_reflect]
-inductive ring_expr : Type
-| add : ring_expr → ring_expr → ring_expr
-| mul : ring_expr → ring_expr → ring_expr
-| const : znum → ring_expr
-| X : ring_expr
+-- We start by modelling polynomials as lists of integers. Note that znum
+-- is basically the same as int, but optimised for computations.
 
-meta def reflect_expr (X : expr) : expr → option ring_expr
-| `(%%e₁ + %%e₂) := do
-  p₁ ← reflect_expr e₁,
-  p₂ ← reflect_expr e₂,
-  return (ring_expr.add p₁ p₂)
-| `(%%e₁ * %%e₂) := do
-  p₁ ← reflect_expr e₁,
-  p₂ ← reflect_expr e₂,
-  return (ring_expr.mul p₁ p₂)
-| e := if e = X then return ring_expr.X else
-  do n ← expr.to_int e,
-     return (ring_expr.const (znum.of_int' n))
+-- The list [a0,a1,...,an] represents a0 + a1*x + a2*x^2 + ... +an*x^n
 
 def poly := list znum
+
+-- We now make basic definitions and prove basic lemmas about addition of polynomials,
+-- multiplication of a polynomial by a scalar, and multiplication of polynomials.
 
 def poly.add : poly → poly → poly
 | [] g := g
@@ -45,6 +32,9 @@ def poly.const : znum → poly := λ z, [z]
 
 def poly.X : poly := [0,1]
 
+-- One problem with our implementation is that the lists [1,2,3] and [1,2,3,0] are different
+-- list, but represent the same polynomial. So we define an "is_equal" predicate.
+
 def poly.is_eq_aux : list znum -> list znum -> bool
 | [] [] := tt 
 | [] (h₂ :: t₂) := if (h₂ = 0) then poly.is_eq_aux [] t₂ else ff 
@@ -53,15 +43,12 @@ def poly.is_eq_aux : list znum -> list znum -> bool
 
 def poly.is_eq : poly → poly → bool := poly.is_eq_aux 
 
-def to_poly : ring_expr → poly
-| (ring_expr.add e₁ e₂) := (to_poly e₁).add (to_poly e₂)
-| (ring_expr.mul e₁ e₂) := (to_poly e₁).mul (to_poly e₂)
-| (ring_expr.const z) := poly.const z
-| ring_expr.X := poly.X
-
+-- evaluation of a polynomial at some element of a commutative ring.
 def poly.eval {α} [comm_ring α] (X : α) : poly → α
 | [] := 0
 | (n::l) := n + X * poly.eval l
+
+-- Lemmas saying that evaluation plays well with addition, multiplication, polynomial equality etc
 
 @[simp] lemma poly.eval_zero {α} [comm_ring α] (X : α) : poly.eval X [] = 0 := rfl
 
@@ -123,6 +110,7 @@ begin
   unfold poly.X poly.eval,simp
 end
 
+-- Different list representing the same polynomials evaluate to the same thing
 theorem poly.eval_is_eq {α} [comm_ring α] (X : α) {p₁ p₂ : poly} : 
   poly.is_eq p₁ p₂ → p₁.eval X = p₂.eval X := 
 begin
@@ -154,23 +142,61 @@ begin
     
 end 
 
+-- That's the end of the poly interface. We now prepare for the reflection.
 
+-- First an abstract version of polynomials (where equality is harder to test, and we won't
+-- need to test it). We'll construct a term of this type from (x+1)*(x+1)*(x+1)
+
+-- fancy attribute because we will be using reflection in meta-land
+@[derive has_reflect]
+inductive ring_expr : Type
+| add : ring_expr → ring_expr → ring_expr
+| mul : ring_expr → ring_expr → ring_expr
+| const : znum → ring_expr
+| X : ring_expr
+
+-- Now a "reflection" of this abstract type which the VM can play with.
+meta def reflect_expr (X : expr) : expr → option ring_expr
+| `(%%e₁ + %%e₂) := do
+  p₁ ← reflect_expr e₁,
+  p₂ ← reflect_expr e₂,
+  return (ring_expr.add p₁ p₂)
+| `(%%e₁ * %%e₂) := do
+  p₁ ← reflect_expr e₁,
+  p₂ ← reflect_expr e₂,
+  return (ring_expr.mul p₁ p₂)
+| e := if e = X then return ring_expr.X else
+  do n ← expr.to_int e,
+     return (ring_expr.const (znum.of_int' n))
+
+-- turning the abstract poly into a concrete list of coefficients.
+def to_poly : ring_expr → poly
+| (ring_expr.add e₁ e₂) := (to_poly e₁).add (to_poly e₂)
+| (ring_expr.mul e₁ e₂) := (to_poly e₁).mul (to_poly e₂)
+| (ring_expr.const z) := poly.const z
+| ring_expr.X := poly.X
+
+-- evaluating the abstract poly
 def ring_expr.eval {α} [comm_ring α] (X : α) : ring_expr → α
 | (ring_expr.add e₁ e₂) := e₁.eval + e₂.eval
 | (ring_expr.mul e₁ e₂) := e₁.eval * e₂.eval
 | (ring_expr.const z) := z
 | ring_expr.X := X
 
+-- evaluating the abstract and the concrete polynomial gives the same answer
 theorem to_poly_eval {α} [comm_ring α] (X : α) (e) : (to_poly e).eval X = e.eval X :=
 by induction e; simp [to_poly, ring_expr.eval, *]
 
---theorem main_thm' {α} [comm_ring α] (X : α) (e₁ e₂) {x₁ x₂}
---  (H : to_poly e₁ = to_poly e₂) (R1 : e₁.eval X = x₁) (R2 : e₂.eval X = x₂) : x₁ = x₂ :=
---by rw [← R1, ← R2, ← to_poly_eval, H, to_poly_eval]
-
+-- The big theorem! If the concrete polys are equal then the abstract ones evaluate
+-- to the same value. 
 theorem main_thm {α} [comm_ring α] (X : α) (e₁ e₂) {x₁ x₂}
   (H : poly.is_eq (to_poly e₁) (to_poly e₂)) (R1 : e₁.eval X = x₁) (R2 : e₂.eval X = x₂) : x₁ = x₂ :=
 by rw [← R1, ← R2, ← to_poly_eval,poly.eval_is_eq X H, to_poly_eval]
+
+-- Now here's the tactic! It takes as input the unknown but concrete variable x
+-- and an expression f(x)=g(x),
+-- creates abstract polys f(X) and g(X), proves they're equal using rfl,
+-- and then applies the main theorem to deduce f(x)=g(x).
 
 meta def ring_tac (X : pexpr) : tactic unit := do
   X ← to_expr X,
