b77375056554dcea75fc8f5d001817e91f8626c1
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-03 23:42:32 +0100

    Q1 tweaks

--- M1F/problem_bank/PB0101/Q0101.lean
+++ M1F/problem_bank/PB0101/Q0101.lean
@@ -1,10 +1,15 @@
-import xenalib.M1F.PB0101
 /-
 M1F 2018-19 Sheet 1 Question 1
 Author : Kevin Buzzard
 
 Tested with lean 3.4.1 and mathlib from September 2018.
 -/
+
+definition quadroots {x : ℝ} : x ^ 2 - 3 * x + 2 = 0 ↔ x = 1 ∨ x = 2 := sorry
+
+-- for proof see https://github.com/kbuzzard/xena/tree/master/M1F/problem_bank/PB0101/PB0101.lean
+
+-- you can rewrite this if you need it.
 
 -- part (a) -- delete one of these and prove the other.
 
