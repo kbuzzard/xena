951f24c6809e53387bb4b17903982a45f618fd5c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-04 14:14:00 +0100

    video stuff

---
+++ blog/video.lean
@@ -0,0 +1,30 @@
+#exit
+
+
+
+    Type
+
+
+
+
+ℝ ℚ ℤ 
+
+   group G
+   ring R
+   modular_form f
+   ...
+
+
+
+   π : ℝ
+
+
+
+   ℝ : Type 
+
+
+
+   ℕ = {0, 1, 2, 3, ...}
+
+
+∀ s : ℂ, ζ(s) = 0 → s ∈ ℤ ∨ Re(s) = 1/2
