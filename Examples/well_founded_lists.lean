7a401d197670d5f6a826642589237254682dd609
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-11-04 09:01:49 +0000

    stuff

--- Examples/well_founded_lists.lean
+++ Examples/well_founded_lists.lean
@@ -1,6 +1,6 @@
 import data.equiv.basic
 
-namespace xena
+namespace wf
 
 inductive W (L : Type) (A : L → Type) : Type
 | Node : ∀ (lbl : L), (A lbl → W) → W
@@ -10,10 +10,31 @@ def nat := W bool (λ b, bool.rec empty unit b)
 def list (L : Type) :=
 W (option L) (λ l, option.rec_on l empty (λ _, unit))
 
-end xena
+end wf
 
-example : equiv nat xena.nat :=
-{ to_fun := λ n, nat.rec_on n (xena.W.Node ff empty.elim) (λ d w, xena.W.Node tt $ λ _, w),
-  inv_fun := λ w, xena.W.rec_on w $ λ b H1 H2, begin cases b, exact 0, exact nat.succ (H2 unit.star) end,
-  left_inv := _,
-  right_inv := _ }
+example : equiv nat wf.nat :=
+{ to_fun := λ n, nat.rec_on n (wf.W.Node ff empty.elim) (λ d w, wf.W.Node tt $ λ _, w),
+  inv_fun := λ w, wf.W.rec_on w $ λ b, bool.rec_on b (λ _ _, 0) (λ H1 H2, nat.succ (H2 ())),
+  left_inv := begin
+    intro n,
+    induction n with d hd, refl,
+    dsimp,
+    congr',
+  end,
+  right_inv := begin
+    intro w,
+    induction w with b H1 H2,
+    cases b,
+    { dsimp,
+      -- congr', -- fails
+      convert rfl, -- works
+      ext x,
+      cases x,
+    },
+    { dsimp,
+      congr',
+      ext a,
+      convert (H2 a)
+    }
+  end
+}
