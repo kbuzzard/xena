a06d131056d93640f4343fc68ee86484e1bf29ee
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-01-22 01:14:19 +0000

    adding how to get bbV

--- Maths_Challenges/challenges/challenge09.lean
+++ Maths_Challenges/challenges/challenge09.lean
@@ -8,6 +8,8 @@ open set
 -- 𝕍 𝕀 𝕍 = 𝕍 for a contravariant Galois connection
 -- for example the one between R=k[X₁,X₂,…,Xₙ] and A=𝔸ⁿ
 -- in the theory of algebraic varieties
+
+-- NB type `\bbV` to get the blackboard bold V, and `\bbI` for the I
 example
   (𝕍_antimono : ∀ J₁ J₂ : set R, J₁ ⊆ J₂ → 𝕍 J₂ ⊆ 𝕍 J₁)
   (𝕀_antimono : ∀ W₁ W₂ : set A, W₁ ⊆ W₂ → 𝕀 W₂ ⊆ 𝕀 W₁)
