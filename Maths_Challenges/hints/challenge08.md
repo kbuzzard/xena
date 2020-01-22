bddbb69f28399ade58c0b845b113ca08e057a896
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-01-22 00:40:28 +0000

    adding challenge 8

---
+++ Maths_Challenges/hints/challenge08.md
@@ -0,0 +1,10 @@
+# Hints for challenge 8.
+
+This came up in the course I'm teaching this term.
+Recall that `set.subset.antisymm` is the theorem
+that if A ⊆ B and B ⊆ A then A = B. If you also
+prove (𝕀 (𝕍 (𝕀 W))) = 𝕀 W (and the proof is the same
+as the challenge of course) then you can deduce that
+the images of 𝕍 and 𝕀 biject with each other, which
+looks a bit like the Nullstellensatz in the context
+we're in, in my course. Why is it not the Nullstellensatz?
---
+++ Maths_Challenges/solutions/challenge08.lean
@@ -0,0 +1,22 @@
+import data.set.basic
+
+variables (R A : Type)
+variables (𝕍 : set R → set A) (𝕀 : set A → set R)
+
+open set
+
+-- 𝕍 𝕀 𝕍 = 𝕍 for a contravariant Galois connection
+-- for example the one between R=k[X₁,X₂,…,Xₙ] and A=𝔸ⁿ
+-- in the theory of algebraic varieties
+example
+  (𝕍_antimono : ∀ J₁ J₂ : set R, J₁ ⊆ J₂ → 𝕍 J₂ ⊆ 𝕍 J₁)
+  (𝕀_antimono : ∀ W₁ W₂ : set A, W₁ ⊆ W₂ → 𝕀 W₂ ⊆ 𝕀 W₁)
+  (galois : ∀ J : set R, ∀ W : set A, J ⊆ 𝕀 W ↔ W ⊆ 𝕍 J) :
+  ∀ J : set R, 𝕍 (𝕀 (𝕍 J)) = 𝕍 J :=
+begin
+  intro J,
+  apply set.subset.antisymm,
+  { apply 𝕍_antimono,
+    rw galois},
+  { rw ←galois}
+end
