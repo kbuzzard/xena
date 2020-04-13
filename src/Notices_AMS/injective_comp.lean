d7c2f099dd6fe0261831d3bc50be21b2a19e3147
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-04-13 23:36:44 +0100

    make spacing better

--- src/Notices_AMS/injective_comp.lean
+++ src/Notices_AMS/injective_comp.lean
@@ -1,18 +1,20 @@
 import tactic -- tactic mode
 open function
 
-variables (X Y Z : Type) (f : X → Y) (g : Y → Z)
+variables (X Y Z : Type)
+  (f : X → Y) (g : Y → Z)
 
 /-- The composite of two injective functions is injective. -/
 theorem injective_comp :
-  injective f ∧ injective g → injective (g ∘ f) :=
+  injective f ∧ injective g →
+  injective (g ∘ f) :=
 begin
   -- assume f and g are injective. We want to prove g ∘ f is injective.
   rintro ⟨f_inj, g_inj⟩,
   -- So say a,b ∈ X and assume g(f(a))=g(f(b)). We want to prove that a = b.
-  intros a b gfa_eq_gfb,
+  intros a b hab,
   -- By injectivity of f, it suffices to prove that f(a)=f(b).
   apply f_inj,
   -- By injectivity of g, and our assumption, we're done.
-  exact g_inj gfa_eq_gfb
+  exact g_inj hab
 end
