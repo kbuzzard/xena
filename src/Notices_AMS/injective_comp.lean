2a73ae4b583c38cf7074aa6d8a241f252fc81730
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-05-01 15:19:17 +0100

    adding comma

--- src/Notices_AMS/injective_comp.lean
+++ src/Notices_AMS/injective_comp.lean
@@ -10,14 +10,14 @@ theorem injective_comp :
   injective f ∧ injective g →
   injective (g ∘ f) :=
 begin
-  -- assume f and g are injective. We want to prove g ∘ f is injective.
+  -- assume f and g are injective. 
   rintro ⟨f_inj, g_inj⟩,
-  -- So say a,b ∈ X and assume g(f(a))=g(f(b)). We want to prove that a = b.
-  intros a b hab,
-  -- By injectivity of f, it suffices to prove that f(a)=f(b).
+  -- We want to prove g ∘ f is injective. So say a,b ∈ X and assume g(f(a))=g(f(b)).
+  intros a b hgf,
+  -- We want to prove that a = b. By injectivity of f, it suffices to prove that f(a)=f(b).
   apply f_inj,
   -- By injectivity of g, it suffices to prove g(f(a))=g(f(b)).
   apply g_inj,
   -- But this is an assumption.
-  assumption
+  assumption,
 end
