ea1972eed60414959f8d0f692257da8b40bca2ce
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-07-20 13:16:57 +0100

    smaller lines

--- src/Notices_AMS/injective_comp.lean
+++ src/Notices_AMS/injective_comp.lean
@@ -1,22 +1,33 @@
-import tactic -- gives access to many useful tactics
+-- need access to many useful tactics
+import tactic 
 
-open function -- definition of injective now available
+-- need injective functions
+open function
 
+-- Let X, Y, Z be types and
+-- let f : X → Y and g : Y → Z be
+-- functions between these types
 variables (X Y Z : Type)
   (f : X → Y) (g : Y → Z)
 
-/-- The composite of two injective functions is injective. -/
+-- The composite of two injective
+-- functions is injective.
 theorem injective_comp :
   injective f ∧ injective g →
   injective (g ∘ f) :=
 begin
   -- assume f and g are injective. 
   rintro ⟨f_inj, g_inj⟩,
-  -- We want to prove g ∘ f is injective. So say a,b ∈ X and assume g(f(a))=g(f(b)).
+  -- We want to prove g ∘ f is
+  -- injective. So say a,b ∈ X and
+  -- assume g(f(a))=g(f(b)).
   intros a b hgf,
-  -- We want to prove that a = b. By injectivity of f, it suffices to prove that f(a)=f(b).
+  -- We want to prove that a = b. By
+  -- injectivity of f, it suffices to
+  -- prove that f(a)=f(b).
   apply f_inj,
-  -- By injectivity of g, it suffices to prove g(f(a))=g(f(b)).
+  -- By injectivity of g, it suffices
+  -- to prove g(f(a))=g(f(b)).
   apply g_inj,
   -- But this is an assumption.
   assumption,
