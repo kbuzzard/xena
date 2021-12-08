7a3ccfaf967d1fb91143df8392b3b2642ad8a8c7
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-12-08 23:25:23 +0000

    add ICM proof of infinititude of primes

---
+++ src/ICM/infinitude_primes.lean
@@ -0,0 +1,8 @@
+-- import definition of prime number
+import data.nat.prime
+
+open nat
+
+-- the claim that there are infinitely many primes
+example : ∀ n, ∃ p, n ≤ p ∧ prime p :=
+exists_infinite_primes -- use the existing proof in Lean's maths library
